e09d001d821b0d227e986983ccc19c947ab45c3a
lety2111 <leticiapaty2012@gmail.com>
2022-08-30 16:50:05 -0300

    Finalizando projeto

---
+++ js/script.js
@@ -0,0 +1,21 @@
+    function AbreWhats(){
+
+        window.open('https://wa.me/message/FPHMINBOFKPCE1');
+    }
+
+     function AbreInsta(){
+
+        window.open('https://instagram.com/imports_br7?igshid=YmMyMTA2M2Y=');
+    }
+
+    function VoltarHome(){
+
+       window.location.href="Home.html";
+    }
+
+    function AbrePag(id){
+        console.log(id)
+
+        window.location.href = 'Camisetas.html?id=' + id;
+
+    }
